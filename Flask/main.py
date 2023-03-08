802976bab132dc60bc48490801635aedf8f9d679
LuisEduardoCantero <canterovl2017@licifug.ugto.mx>
2023-03-08 17:12:33 -0600

    More Flask tools added

--- Flask/main.py
+++ Flask/main.py
@@ -1,9 +1,22 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session, url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
-# creamos una instancia de flask
+
+# creamos una instancia de flask y de Bootstrap
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+app.config['SECRET_KEY'] = 'SUPER_SECRETO'
 
 todos = ['TODO_1', 'TODO_1', 'TODO_4', 'TODO_3', 'TODO_2']
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators = [DataRequired()])
+    password = PasswordField('Password', validators = [DataRequired()])
+    submit = SubmitField('Enviar')
+    
 
 @app.errorhandler(404)
 def not_found(error):
@@ -16,16 +29,45 @@ def index():
     user_ip = request.remote_addr
     
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
     
     return response  
 
-@app.route('/hello') # usamos este decorador para ejecutar hello
+@app.route('/hello', methods = ['GET', 'POST']) # usamos este decorador para ejecutar hello
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
+    login_form = LoginForm()
+    username = session.get('username')
     context = {
         'user_ip':user_ip,
         'todos': todos,
+        'login_form': login_form,
+        'username': username
     }
     
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+        flash('Nombre registrado con exito!')
+        
+        return redirect(url_for('index'))
+    
     return render_template('hello.html', **context)
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
